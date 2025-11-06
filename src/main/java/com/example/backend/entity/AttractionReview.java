321ea558a8e0a20635e0dc76be2a0b4951f0ec6a
박찬혁 <cksgurrhtn@naver.com>
2025-11-06 14:50:36 +0900

    review dto 추가, 내용 수정

--- src/main/java/com/example/backend/entity/AttractionReview.java
+++ src/main/java/com/example/backend/entity/AttractionReview.java
@@ -1,13 +1,13 @@
 package com.example.backend.entity;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class AttractionReview {
     private Long id;
