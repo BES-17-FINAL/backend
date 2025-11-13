0eb5ec7af7a0fa83ce5829958ba59e40f4b8422a
sy18346 <yutubesso994@gmail.com>
2025-11-13 17:48:31 +0900

    대댓글 기능 추가

--- src/main/java/com/example/backend/repository/CommentRepository.java
+++ src/main/java/com/example/backend/repository/CommentRepository.java
@@ -6,12 +6,19 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    Page<Comment> findByPostIdAndDeletedAtIsNull(Long postId, Pageable pageable);
+    Page<Comment> findByPostIdAndParentIsNull(Long postId, Pageable pageable);
+
+    List<Comment> findByParentIdOrderBySortOrderAsc(Long parentId);
+
+    Long countByPostIdAndParentIsNullAndDeletedAtIsNull(Long postId);
+
+    Long countByParentIdAndDeletedAtIsNull(Long parentId);
 
     Optional<Comment> findByIdAndDeletedAtIsNull(Long commentId);
 
