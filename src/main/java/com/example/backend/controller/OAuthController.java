4be5d4fbdae3bf8f941cbc09b89fd238f4c8bb6c
박찬혁 <cksgurrhtn@naver.com>
2025-11-17 18:12:53 +0900

    내정보 리뷰 내역 확인

--- src/main/java/com/example/backend/controller/OAuthController.java
+++ src/main/java/com/example/backend/controller/OAuthController.java
@@ -2,12 +2,10 @@ package com.example.backend.controller;
 
 import com.example.backend.service.OAuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 @RequiredArgsConstructor
@@ -15,11 +13,10 @@ public class OAuthController {
 
     private final OAuthService oAuthService;
 
-
     @GetMapping("/oauth/success")
-    public String oauthSuccess(Authentication authentication,
-                               @RequestParam(name = "provider", defaultValue = "unknown") String provider) {
-        OAuth2User oAuthUser = (OAuth2User) authentication.getPrincipal();
+    public String oauthSuccess(OAuth2AuthenticationToken authToken) {
+        String provider = authToken.getAuthorizedClientRegistrationId();
+        OAuth2User oAuthUser = authToken.getPrincipal();
         String jwt = oAuthService.processOAuthLogin(oAuthUser, provider);
         return "OAuth 로그인 성공! JWT 토큰: " + jwt;
     }
