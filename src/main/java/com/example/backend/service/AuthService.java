d95065b8622f91e2bb82137e465a851807795c63
박찬혁 <cksgurrhtn@naver.com>
2025-11-21 17:15:20 +0900

    로그인 부분 오류

--- src/main/java/com/example/backend/service/AuthService.java
+++ src/main/java/com/example/backend/service/AuthService.java
@@ -8,8 +8,10 @@ import com.example.backend.repository.UserRepository;
 import com.example.backend.security.JwtUtil;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -29,36 +31,37 @@ public class AuthService {
         return "회원가입 완료";
     }
 
+    // 로그인
     public LoginResponse login(LoginRequest request) {
         User user = userRepository.findByEmail(request.getEmail())
-                .orElseThrow(() -> new RuntimeException("이메일이 존재하지 않습니다."));
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "이메일이 존재하지 않습니다."));
+
         if (!encoder.matches(request.getPassword(), user.getPassword())) {
-            throw new RuntimeException("비밀번호가 올바르지 않습니다.");
+            throw new ResponseStatusException(
+                    HttpStatus.UNAUTHORIZED, "비밀번호가 올바르지 않습니다.");
         }
 
         String token = jwtUtil.generateToken(user.getEmail());
         return new LoginResponse(token, user.getNickname(), user.getEmail());
     }
+
+    // 비밀번호 변경
     @Transactional
     public void changePassword(String token, ChangePasswordRequest request) {
-
-        // Authorization: Bearer XXX → XXX
         String jwt = token.replace("Bearer ", "");
-
-        // 토큰에서 email 추출
         String email = jwtUtil.extractEmail(jwt);
 
         User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."));
 
-        // 현재 비밀번호 검증
         if (!encoder.matches(request.getCurrentPassword(), user.getPassword())) {
-            throw new RuntimeException("현재 비밀번호가 일치하지 않습니다.");
+            throw new ResponseStatusException(
+                    HttpStatus.UNAUTHORIZED, "현재 비밀번호가 일치하지 않습니다.");
         }
 
-        // 새 비밀번호 암호화 후 저장
         user.setPassword(encoder.encode(request.getNewPassword()));
         userRepository.save(user);
     }
-
 }
