6d7b99bf83344bd4c78068cb4333b1418cf65d78
crado00 <alrlawowns@gmail.com>
2025-11-24 01:56:23 +0900

    프로필 이미지 업데이트

--- src/main/java/com/example/backend/service/UserService.java
+++ src/main/java/com/example/backend/service/UserService.java
@@ -6,13 +6,14 @@ import com.example.backend.entity.User;
 import com.example.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
-
+    private final FileUploadService fileUploadService;
     public Long resolveUserId(Long userId, String email){
         if (userId != null) return userId;
         if (email == null) throw new IllegalArgumentException("UNAUTHORIZED");
@@ -41,5 +42,25 @@ public class UserService {
         if (req.getProfile_img() != null) u.setProfileImg(req.getProfile_img());
 
         userRepository.save(u);
+    }
+
+    public void userProfileImage(MultipartFile file, Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("USER_NOT_FOUND"));
+
+        String image = user.getProfileImg();
+        System.out.println(image != null);
+        System.out.println(!image.isEmpty());
+        System.out.println(file != null);
+        System.out.println(!file.isEmpty());
+        if (image != null && !image.isEmpty()) {
+            fileUploadService.deleteImage(image);
+        }
+        if (file != null && !file.isEmpty()){
+            String uploadedUrl = fileUploadService.uploadImage(file);
+            user.setProfileImg(uploadedUrl);
+            userRepository.save(user);
+        }
+
     }
 }
